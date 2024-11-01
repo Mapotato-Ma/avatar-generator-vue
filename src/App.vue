8633a213ca626b9066da3d9c76ffe5d25c11e153
Mapotato-Ma <pf-ma@qq.com>
2024-11-01 23:10:55 +0800

    Changes

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,29 @@
   <div class="home">
     <div class="slide">
       <div
-        class="part"
-        :class="{ active: item.key === currentPart }"
-        @click="switchPart(item.key)"
-        v-for="item in [{ key: 'bg', label: '背景' }, ...parts]"
+          class="part"
+          :class="{ active: item.key === currentPart }"
+          @click="switchPart(item.key)"
+          v-for="item in [{ key: 'bg', label: '背景' }, ...parts]"
       >
         {{ item.label }}
       </div>
     </div>
     <div class="view" id="view" :style="{ backgroundColor: bgColor }">
       <div
-        :class="[key]"
-        v-for="{ key, position: { x, y } } in parts"
-        :id="key"
-        v-html="getPart(key)"
-        :style="{ left: `${x}px`, top: `${y}px` }"
+          :class="[key]"
+          v-for="{ key, position: { x, y } } in parts"
+          :id="key"
+          v-html="getPart(key)"
+          :style="{ left: `${x}px`, top: `${y}px` }"
       ></div>
       <video ref="selfie" width="300" height="300"></video>
       <img
-        v-if="selectedParts.get('face')?.includes('data:image/png;base64')"
-        :src="selectedParts.get('face')"
-        width="300"
-        height="300"
-      />
+          v-if="selectedParts.get('face')?.includes('data:image/png;base64')"
+          :src="selectedParts.get('face')"
+          width="300"
+          height="300"
+          alt="脸"/>
       <canvas ref="canvas" width="300" height="300"></canvas>
     </div>
     <div class="resources">
@@ -34,50 +34,51 @@
         <i class="icon-stop" @click="stopRecording"></i>
       </div>
       <div
-        class="icon"
-        :class="{ active: selectedParts.get(currentPart) === '' }"
-        @click="selectedParts.set(currentPart, '')"
-        v-if="currentPart !== 'bg'"
+          class="icon"
+          :class="{ active: selectedParts.get(currentPart) === '' }"
+          @click="selectedParts.set(currentPart, '')"
+          v-if="currentPart !== 'bg'"
       ></div>
       <div class="icon icon-bg-select" :style="{ backgroundColor: bgColor }" v-if="currentPart === 'bg'">
-        <input type="color" name="bg" id="bg" v-model="bgColor" />
+        <input type="color" name="bg" id="bg" v-model="bgColor"/>
       </div>
       <div
-        class="icon icon-bg"
-        :class="{ active: bgColor === color }"
-        v-for="color in BG_COLORS"
-        :key="color"
-        :style="{ backgroundColor: color }"
-        @click="bgColor = color"
-        v-show="currentPart === 'bg'"
+          class="icon icon-bg"
+          :class="{ active: bgColor === color }"
+          v-for="color in BG_COLORS"
+          :key="color"
+          :style="{ backgroundColor: color }"
+          @click="bgColor = color"
+          v-show="currentPart === 'bg'"
       ></div>
       <div
-        class="icon"
-        :class="{ active: selectedParts.get(currentPart) === iconName }"
-        v-for="[iconName, iconSvg] in Object.entries(currentLibrary)"
-        :key="iconName"
-        v-html="iconSvg"
-        @click="selectedParts.set(currentPart, iconName)"
+          class="icon"
+          :class="{ active: selectedParts.get(currentPart) === iconName }"
+          v-for="[iconName, iconSvg] in Object.entries(currentLibrary)"
+          :key="iconName"
+          v-html="iconSvg"
+          @click="selectedParts.set(currentPart, iconName)"
       ></div>
       <div
-        class="icon"
-        :class="{ active: selectedParts.get(currentPart) === png }"
-        v-for="png in storage"
-        v-show="currentPart === 'face'"
-        @click="selectedParts.set('face', png)"
+          class="icon"
+          :class="{ active: selectedParts.get(currentPart) === png }"
+          v-for="png in storage"
+          v-show="currentPart === 'face'"
+          @click="selectedParts.set('face', png)"
       >
-        <img :src="png" alt="" srcset="" />
+        <img :src="png" alt="" srcset=""/>
       </div>
     </div>
     <div class="btn" @click="exportAvatarWithPng"></div>
   </div>
 </template>
 <script setup lang="ts">
-import { computed, onMounted, ref, useTemplateRef } from 'vue';
-import { iconMap } from './assets/icons';
-import { BG_COLORS } from './common/const';
-import { useStorage } from '@vueuse/core';
-import { Screenshot } from './common/screenshot.service';
+import {computed, onMounted, ref, useTemplateRef} from 'vue';
+import {iconMap} from './assets/icons';
+import {BG_COLORS} from './common/const';
+import {useStorage} from '@vueuse/core';
+import {Screenshot} from './common/screenshot.service';
+
 const canvasEl = useTemplateRef('canvas');
 const selfieEl = useTemplateRef('selfie');
 const currentPart = ref('face');
@@ -91,26 +92,26 @@ onMounted(() => {
 });
 
 const parts = [
-  { key: 'face', label: '脸', position: { x: 43, y: 60 } },
-  { key: 'eyes', label: '眼睛', position: { x: 144, y: 110 } },
-  { key: 'mouth', label: '嘴巴', position: { x: 160, y: 173 } },
-  { key: 'clothes', label: '着装', position: { x: 14, y: 191 } },
-  { key: 'decoration', label: '配饰', position: { x: 90, y: 123 } },
-  { key: 'hair', label: '头发', position: { x: 26, y: 40 } },
+  {key: 'face', label: '脸', position: {x: 43, y: 60}},
+  {key: 'eyes', label: '眼睛', position: {x: 144, y: 110}},
+  {key: 'mouth', label: '嘴巴', position: {x: 160, y: 173}},
+  {key: 'clothes', label: '着装', position: {x: 14, y: 191}},
+  {key: 'decoration', label: '配饰', position: {x: 90, y: 123}},
+  {key: 'hair', label: '头发', position: {x: 26, y: 40}},
 ];
 const currentLibrary = computed(() => {
   return iconMap[currentPart.value] ?? {};
 });
 const selectedParts = ref<Map<string, string | undefined>>(
-  new Map([
-    ['decoration', '眼镜'],
-    ['hair', '15'],
-    ['mouth', '露牙齿'],
-    ['eyes', '悲伤'],
-    ['clothes', '时尚夹克'],
-    ['face', '白皙肤色'],
-    ['mask', undefined],
-  ])
+    new Map([
+      ['decoration', '眼镜'],
+      ['hair', '15'],
+      ['mouth', '露牙齿'],
+      ['eyes', '悲伤'],
+      ['clothes', '时尚夹克'],
+      ['face', '白皙肤色'],
+      ['mask', undefined],
+    ])
 );
 const getPart = (part: string) => iconMap[part]?.[selectedParts.value.get(part) ?? ''] ?? '';
 const switchPart = (part: string) => (currentPart.value = part);
@@ -129,17 +130,17 @@ const exportAvatarWithPng = async () => {
   const promises: Promise<void>[] = [];
   for (const svgElement of svgElements) {
     const [top, left] = [svgElement.parentElement!.style.top, svgElement.parentElement!.style.left].map((v) =>
-      parseInt(v)
+        parseInt(v)
     );
     const svgString = new XMLSerializer().serializeToString(svgElement);
     const img = new Image();
     promises.push(
-      new Promise<void>((resolve) => {
-        img.onload = function () {
-          draw.drawImage(img, left, top);
-          resolve();
-        };
-      })
+        new Promise<void>((resolve) => {
+          img.onload = function () {
+            draw.drawImage(img, left, top);
+            resolve();
+          };
+        })
     );
     img.src = 'data:image/svg+xml,' + encodeURIComponent(svgString);
   }
@@ -160,15 +161,15 @@ const recording = () => {
   if (playing.value) {
     return;
   }
-  navigator.mediaDevices.getUserMedia({ video: { width: 300, height: 300 }, audio: false }).then(
-    (stream) => {
-      selfieEl.value!.srcObject = stream;
-      selfieEl.value?.play();
-      playing.value = true;
-    },
-    () => {
-      alert('无法获取您的美照');
-    }
+  navigator.mediaDevices.getUserMedia({video: {width: 300, height: 300}, audio: false}).then(
+      (stream) => {
+        selfieEl.value!.srcObject = stream;
+        selfieEl.value?.play();
+        playing.value = true;
+      },
+      () => {
+        alert('无法获取您的美照');
+      }
   );
 };
 
@@ -193,7 +194,6 @@ const screenshot = () => {
   padding: 2rem;
   border-radius: 2rem;
   max-width: 800px;
-  min-width: 500px;
   height: 26rem;
   outline: 100px solid #ffc400;
   display: grid;
@@ -202,6 +202,7 @@ const screenshot = () => {
   gap: 2em;
   background: #636363;
   min-width: max-content;
+
   .btn {
     cursor: pointer;
     position: absolute;
@@ -213,6 +214,7 @@ const screenshot = () => {
     background: aliceblue;
     border-radius: 100%;
     background: linear-gradient(45deg, #825dfd, #2f2f2f);
+
     &::after {
       content: '';
       position: absolute;
@@ -221,6 +223,7 @@ const screenshot = () => {
       border-radius: 3px;
       transition: box-shadow 0.2s;
     }
+
     &:active::after {
       box-shadow: 0 0 20px 1px #f9e298;
     }
@@ -240,11 +243,13 @@ const screenshot = () => {
     cursor: pointer;
     border-radius: 0.2rem;
     padding: 0.2rem 0.5rem;
-    outline: 2.5px solid #ffc400;
+    outline: 3px solid #ffc400;
     transition: background-color 0.2s;
+
     &:hover {
       background-color: #ffc400a9;
     }
+
     &.active {
       background-color: #ff8800;
     }
@@ -258,9 +263,11 @@ const screenshot = () => {
   border-radius: 100%;
   border: 3px solid rgba(255, 196, 0, 0.7);
   overflow: hidden;
+
   canvas {
     display: none;
   }
+
   video,
   img {
     position: absolute;
@@ -285,7 +292,7 @@ const screenshot = () => {
   grid-auto-rows: minmax(3rem, auto);
   align-content: flex-start;
   align-items: stretch;
-  overflow: auto;
+
   .icon {
     display: flex;
     align-items: center;
@@ -295,9 +302,10 @@ const screenshot = () => {
     width: 100%;
     aspect-ratio: 1.1;
     border-radius: 1rem;
-    border: 2.5px solid #ffc400;
+    border: 3px solid #ffc400;
     transition: background-color 0.2s;
     overflow: hidden;
+
     &.icon-bg-select {
       position: sticky;
       top: 0;
@@ -306,29 +314,37 @@ const screenshot = () => {
       aspect-ratio: 3;
       border-radius: 0.2rem;
     }
+
     &.icon-bg {
       border-radius: 0.2rem;
     }
+
     &:has(input) {
       padding: 0;
     }
+
     input {
       opacity: 0;
     }
+
     &:hover {
       background-color: #ffc400a9;
     }
+
     &.active {
       background-color: #ff8800;
     }
+
     &:has(img) {
       padding: 0;
     }
+
     img {
       width: 100%;
       height: 100%;
       object-fit: cover;
     }
+
     &.icon-camera {
       position: sticky;
       top: 0;
@@ -340,9 +356,11 @@ const screenshot = () => {
       justify-content: space-between;
       border: none;
       backdrop-filter: blur(28px);
+
       &:hover {
         background-color: transparent;
       }
+
       i {
         width: 2rem;
         aspect-ratio: 1;
@@ -350,26 +368,16 @@ const screenshot = () => {
         background-color: #ff8800;
         display: grid;
         place-content: center;
+
         &.icon-stop {
           background-color: #fff;
           border: 7px solid #ff8800;
         }
+
         &:hover {
           opacity: 0.6;
         }
       }
-    }
-  }
-  .icons {
-    grid-column: span 2;
-    display: grid;
-    gap: 0.3rem;
-    flex: auto;
-    overflow: auto;
-    grid-template-columns: repeat(2, 1fr);
-    .icon {
-      aspect-ratio: 2;
-      border-radius: 0.2rem;
     }
   }
 }
